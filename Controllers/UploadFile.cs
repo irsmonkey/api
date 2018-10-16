e398e7bb77104a42e9f4db5ea979a58746d045f5
Gerardo Jaramillo <gerardo.jaramillo@gmail.com>
2018-10-16 13:38:21 -0600

    Upload file with name and folder creation

--- Controllers/UploadFile.cs
+++ Controllers/UploadFile.cs
@@ -27,7 +27,9 @@ namespace IrsMonkeyApi.Controllers
                     _hostingEnvironment.WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "./uploads");
                 }
                 var file = Request.Form.Files[0];
-                var folderName = "uploads";
+                var memberId = Request.Form["memberId"].ToString();
+                var resolution = Request.Form["resolution"].ToString();
+                var folderName = memberId;
                 var webRootPath = _hostingEnvironment.WebRootPath;
                 var newPath = Path.Combine(webRootPath, folderName);
                 if (!Directory.Exists(newPath))
@@ -38,7 +40,8 @@ namespace IrsMonkeyApi.Controllers
                 if (file.Length <= 0) return Json("Upload Successful.");
                 var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.ToString()
                     .Trim('"');
-                var fullPath = Path.Combine(newPath, fileName);
+                var filename2 = resolution + "-" + memberId + "-" + fileName;
+                var fullPath = Path.Combine(newPath, filename2);
                 using (var stream = new FileStream(fullPath, FileMode.Create))
                 {
                     file.CopyTo(stream);
