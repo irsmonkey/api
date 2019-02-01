6c89a91eef30fea9b79ba4a22271e7debe538749
Gerardo Jaramillo <gerardo.jaramillo@gmail.com>
2019-01-31 18:44:16 -0600

    removal of string connection

--- Program.cs
+++ Program.cs
@@ -21,40 +21,5 @@ namespace IrsMonkeyApi
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-        
-//        public static void Main(string[] args)
-//        {
-//            var config = new ConfigurationBuilder()
-//                .SetBasePath(Directory.GetCurrentDirectory())
-//                .AddEnvironmentVariables()
-//                .AddJsonFile("certificate.json", optional: true, reloadOnChange: true)
-//                .AddJsonFile($"certificate.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
-//                .Build();
-//
-//            var certificateSettings = config.GetSection("certificateSettings");
-//            string certificateFileName = certificateSettings.GetValue<string>("filename");
-//            string certificatePassword = certificateSettings.GetValue<string>("password");
-//
-//            var certificate = new X509Certificate2(certificateFileName, certificatePassword);
-//        
-//            var host = new WebHostBuilder()
-//                .UseKestrel(
-//                    options =>
-//                    {
-//                        options.AddServerHeader = false;
-//                        options.Listen(IPAddress.Loopback, 5001, listenOptions =>
-//                        {
-//                            listenOptions.UseHttps(certificate);
-//                        });
-//                    }
-//                )
-//                .UseConfiguration(config)
-//                .UseContentRoot(Directory.GetCurrentDirectory())
-//                .UseStartup<Startup>()
-//                .UseUrls("https://localhost:5001")
-//                .Build();
-//
-//            host.Run();
-//        }
     }
 }
